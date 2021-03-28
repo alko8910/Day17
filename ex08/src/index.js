03a4a6ffa5af0056c2963c35c819390526e454f1
alko8910 <almedinemkic@gmail.com>
2021-03-28 23:47:43 +0200

    Ispravka rjesenja

--- ex08/src/index.js
+++ ex08/src/index.js
@@ -1,46 +1,47 @@
 // Lodash
-const _ = require("lodash");
+const  = require('lodash');
 
 // Only change code below this line
 // users nested array with four objects starts here
-let users = [
-  { firstName: "John", lastName: "Doe", age: 24, gender: "male" },
-  { firstName: "Jane", lastName: "Doe", age: 5, gender: "female" },
-  { firstName: "Jim", lastName: "Carrey", age: 54, gender: "male" },
-  { firstName: "Kate", lastName: "Winslet", age: 40, gender: "female" },
+var users = [
+  { firstName: 'John', lastName: 'Doe', age: 24, gender: 'male' },
+  { firstName: 'Jane', lastName: 'Doe', age: 5, gender: 'female' },
+  { firstName: 'Jim', lastName: 'Carrey', age: 54, gender: 'male' },
+  { firstName: 'Kate', lastName: 'Winslet', age: 40, gender: 'female' },
 ];
 // users nested array with four objects ends here
 
+
 // getUsers function - list of users starts here
 function getUsers() {
-  var output = "";
-  for (var i = 0; i < users.length; i++) {
-    output += `${users[i].firstName} ${users[i].lastName} is ${users[i].age}, ${users[i].gender} \n`;
-  }
-  return output;
+      var output = "";
+      for (let i = 0; i < users.length; i++) {
+        output += ${users[i].firstName} ${users[i].lastName} is ${users[i].age}, ${users[i].gender}\n;
+      }
+      console.log(output);
+      return output;
 }
 
 // getUsers function - list of users ends here
 
+
 // findUser (lastName, gender) function starts here
+
 
 function findUser(lastName, gender) {
   try {
-    // add approprate code here
-    var user = _.filter(users, { lastName: lastName, gender: gender });
-    var iFindUser = `${user[0].firstName} ${user[0].lastName} is ${user[0].age}, ${user[0].gender}`;
+    // add appropriate code here
+    var user = .find(users, { lastName: lastName, gender: gender });
+    var iFindUser = ${user.firstName} ${user.lastName} is ${user.age}, ${user.gender};
 
+    console.log(iFindUser);
     return iFindUser;
   } catch (error) {
     console.log("Cannot read property 'firstName' of undefined"); // Change this line
     return "Cannot read property 'firstName' of undefined"; // Change this line
-    
   }
 }
-
 // findUser(lastName, gender) function ends here
 // Only change code above this line
-
 getUsers();
-findUser("Carrey", "male"); // Change this line
-module.exports = findUser;
+findUser("Doe", "male"); // Change this line
